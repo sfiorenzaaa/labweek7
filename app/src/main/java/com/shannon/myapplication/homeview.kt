5b1952611463e20ee55d2ec2ff8313194676f9c1
sharonnatalies <sharonsutanto25@gmail.com>
2025-10-22 09:37:33 +0700

    Update homeview.kt

--- app/src/main/java/com/shannon/myapplication/homeview.kt
+++ app/src/main/java/com/shannon/myapplication/homeview.kt
@@ -10,7 +10,7 @@ import com.shannon.myapplication.ui.theme.MyApplicationTheme
 fun HomeView(){
     Column {
         Text (
-            text = "hello shannon!!!"
+            text = "hello mmmmmmmmmmmmmm!!!"
 
         )
         Text (
