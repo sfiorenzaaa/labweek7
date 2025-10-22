7435511c42dbd3fcd6901fc78a621f2be634afb8
sharonnatalies <sharonsutanto25@gmail.com>
2025-10-22 09:27:07 +0700

    creating homeview
    making the home view page

---
+++ app/src/main/java/com/shannon/myapplication/homeview.kt
@@ -0,0 +1,24 @@
+package com.shannon.myapplication
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.shannon.myapplication.ui.theme.MyApplicationTheme
+
+@Composable
+fun HomeView(){
+    Column {
+        Text (
+            text = "hello shannon!!!"
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun HomeViewPreview() {
+    MyApplicationTheme {
+        HomeView()
+    }
+}
